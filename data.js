e1342da77c3a80f9f241a71bfcfd60d71adc76d5
Ikechukwu Chiemelie Charles <chiemeliecharleschristopher@gmail.com>
2025-02-11 13:42:53 +0100

    built the login , signup and mobile menu

---
+++ data.js
@@ -0,0 +1,12 @@
+const mobille = document.querySelector(".mobille");
+
+const ham = document.querySelector(".ham");
+
+const closeMobile = document.querySelector(".closeMobile");
+
+ham.addEventListener("click", function () {
+  mobille.classList.toggle("hide");
+});
+closeMobile.addEventListener("click", function () {
+  mobille.classList.add("hide");
+});
