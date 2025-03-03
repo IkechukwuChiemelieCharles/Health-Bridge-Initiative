b065fb856227a90abbfbc6dbe3f0cdec11fca475
Ikechukwu Chiemelie Charles <chiemeliecharleschristopher@gmail.com>
2025-03-03 17:09:40 +0100

    implemented map

--- PHC Locator/locator.js
+++ PHC Locator/locator.js
@@ -78,8 +78,24 @@ edit.forEach(function (edit, i) {
   });
 });
 
-
 ///////////////////////************MAP************88///////////////////////*
 // /
 
+//Mobile toggle
 
+const darkOverlay2 = document.querySelector(".darkOverlay");
+const mobile2 = document.querySelector(".mobille");
+
+const ham2 = document.querySelector(".ham");
+
+ham2.addEventListener("click", function () {
+  mobile2.classList.add("openmobile");
+  darkOverlay2.classList.remove("hide");
+
+  console.log("ham");
+});
+
+darkOverlay2.addEventListener("click", function () {
+  mobile2.classList.remove("openmobile");
+  darkOverlay2.classList.add("hide");
+});
